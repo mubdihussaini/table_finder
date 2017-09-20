43a07f399c6e2da69a6b3724f475dd3b9b868656
kevin chao <kchao562@gmail.com>
2017-09-19 20:27:51 -0700

    modified server.js

--- server.js
+++ server.js
@@ -13,22 +13,22 @@ app.use(bodyParser.json({type:"application/vnd/api+json"}));
 
 
 var reservations = [
-    // {
-    //   customerName : "yoda",
-    //   phoneNumber  : "991-991-9991",
-    //   email        : "JediMaster@starwars.com",
-    //   partySize    : 1
-    // }
+    {
+      customerName : "yoda",
+      phoneNumber  : "991-991-9991",
+      email        : "JediMaster@starwars.com",
+      partySize    : 1
+    }
   ];
 
-  var waitlists = [
-    // {
-    //   customerName : "yodaAgain",
-    //   phoneNumber  : "991-991-9991",
-    //   email        : "JediMaster@starwars.com",
-    //   partySize    : 1
-    // }
-  ];
+var waitlists = [
+  {
+    customerName : "yodaAgain",
+    phoneNumber  : "991-991-9991",
+    email        : "JediMaster@starwars.com",
+    partySize    : 1
+  }
+];
 
 
 app.get("/", function(req, res) {
@@ -41,6 +41,14 @@ app.get("/tables", function(req, res) {
 
 app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/api/", function(req, res) {
+  var tmpObj = {
+    reservations : reservations,
+    waitlists    : waitlists
+  };
+  return res.json(tmpObj);
 });
 
 app.post("/api/new", function(req, res) {
